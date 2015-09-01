3ca36162f0596ed74a14b58b740a12da7d7a9796
Anna Gilbert <annacg@umich.edu>
2015-09-01 16:03:48 -0400

    added initial comment to code

--- sample_shattering.m
+++ sample_shattering.m
@@ -1,4 +1,12 @@
 function z = sample_shattering(samples, Lambda, t, sig, N)
+	% Input
+	% samples : a set of samples from the signal
+	% Lambda : data structure containing current frequencies and coefficients
+	% t : one random offset (for arithmetic progression)
+	% sig : one random step-size (for arithmetic progression)
+	% N : signal length (should be a power of two)
+	%
+	% Anna C. Gilbert
 	
 	z = sample_residual(samples, Lambda, t, sig, N);
 	n = length(z);
