26e13c715994b1a2d6e6003eb2399f61f2a95f20
Anna Gilbert <annacg@umich.edu>
2015-09-01 16:16:01 -0400

    added initial comments.

--- sample_shattering.m
+++ sample_shattering.m
@@ -6,6 +6,8 @@ function z = sample_shattering(samples, Lambda, t, sig, N)
 	% sig : one random step-size (for arithmetic progression)
 	% N : signal length (should be a power of two)
 	%
+	% Output : a vector z that is the FFT of samples from residual
+	%
 	% Anna C. Gilbert
 	
 	z = sample_residual(samples, Lambda, t, sig, N);
